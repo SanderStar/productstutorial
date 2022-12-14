b78d9e8f78f4ed416a23160ba57c603fae401f55
Sander Star <sanderstar1@gmail.com>
2022-12-14 11:40:25 +0100

    Runtime library for local testing

---
+++ uimodule/webapp/WorkzoneLibrary-content/library-dbg.js
@@ -0,0 +1,47 @@
+/*!
+ * ${copyright}
+ */
+
+/**
+ * Initialization Code and shared classes of library nl.gasunie.workzone.library.
+ */
+sap.ui.define(["sap/ui/core/library"], // library dependency
+	function () {
+
+		"use strict";
+
+		/**
+		 * Demo Lib
+		 *
+		 * @namespace
+		 * @name nl.gasunie.workzone.library
+		 * @author SAP SE
+		 * @version 1.0.0
+		 * @public
+		 */
+
+		// delegate further initialization of this library to the Core
+		sap.ui.getCore().initLibrary({
+			name: "nl.gasunie.workzone.library",
+			version: "1.0.0",
+			dependencies: ["sap.ui.core"],
+			noLibraryCSS: true,
+			types: [],
+			interfaces: [],
+			controls: [
+				"nl.gasunie.workzone.library.controls.Example"
+			],
+			elements: []
+		});
+
+		const oLibrary = nl.gasunie.workzone.library;
+
+		oLibrary.getHelloWorld = () => {
+			return "Hello world part IV";
+		};
+
+		/* eslint-disable */
+		return oLibrary;
+		/* eslint-enable */
+
+	}, /* bExport= */ false);
---
+++ uimodule/webapp/WorkzoneLibrary-content/library.js
@@ -0,0 +1,4 @@
+/*!
+ * ${copyright}
+ */
+sap.ui.define(["sap/ui/core/library"],function(){"use strict";sap.ui.getCore().initLibrary({name:"nl.gasunie.workzone.library",version:"1.0.0",dependencies:["sap.ui.core"],noLibraryCSS:true,types:[],interfaces:[],controls:["nl.gasunie.workzone.library.controls.Example"],elements:[]});const e=nl.gasunie.workzone.library;e.getHelloWorld=()=>"Hello world part IV";return e},false);
